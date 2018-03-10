dd25091c90eb83c9c084a28d5f9bef5dd98184eb
hzzhaoyang3 <hzzhaoyang@corp.netease.com>
2018-03-10 15:32:52 +0800

    asdas

--- src/main/java/com/trail/print/App.java
+++ src/main/java/com/trail/print/App.java
@@ -1,13 +1,25 @@
 package com.trail.print;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author misery.zhao
  * @created 2018/3/9.
  * @project demoprint
  */
 
+@RestController
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
 }
