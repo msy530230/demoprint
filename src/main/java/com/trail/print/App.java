8cb8982b324ef16843ecd7c3b3a513aae31c8d31
hzzhaoyang3 <hzzhaoyang@corp.netease.com>
2018-03-10 11:25:31 +0800

    【aaaa】

--- src/main/java/com/trail/print/App.java
+++ src/main/java/com/trail/print/App.java
@@ -1,18 +1,13 @@
 package com.trail.print;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author misery.zhao
  * @created 2018/3/9.
  * @project demoprint
  */
 
-@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
     }
 
 }
